4397656a3ccf9c3c1831490380be5d46834c3bf6
Romancier <romancier1844@gmail.com>
2025-06-24 20:16:28 +0200

    Comment problem in Desktop-Switcher

--- src/Startup.ps1
+++ src/Startup.ps1
@@ -1,14 +1,15 @@
 	Set-Location $PSScriptRoot # Si wPD_Run_From_Source, peut différer de $env:LocalAppData\wProjectDesktop
 	Set-Location ..
-
 & $env:ahk_wPD .\src\hotkey.ahk
-
 . .\src\New-Project.ps1
 . .\src\Show-Term.ps1
 $configuredProjects = $ProjectConfigs.Keys
 $LastDesktop = Get-CurrentDesktop
 Get-DesktopList | Where-Object { $configuredProjects -contains $_.Name } | foreach {
-	Switch-Desktop -Desktop $_.Name # Puisque $_ n'est pas un desktop object, apparemment
+	Switch-Desktop -Desktop $_.Name
+	# Tourne infiniement parfois ?!
+	# Puisque $_ n'est pas un desktop object, apparemment
+
     New-Project $_.Name
 	Start-Sleep 1.5 # For the program to open before switching to the next desktop... 1/2 (Thunderbird)
 }
@@ -19,5 +20,4 @@ Switch-Desktop $LastDesktop # Windows starts on the desktop that was used when i
 # Windows semble persister les virtual desktops au reboot. Donc je ne les crée qu'une fois au début, dans 'win..ps1'.
 
 # Switch-Desktop -Desktop "docs" # Ce sera le plus utilisé. Mais voyons si Windows se souvient que j'étais dessus en quittant.
-
 . .\src\Start-Term.ps1
