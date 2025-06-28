d1a734db122efb3f0afdde9f188764747d9d21c7
Romancier <romancier1844@gmail.com>
2025-06-28 19:49:22 +0200

    Remove "create new project" command
    Because now the config supports multiple project directories. We could
    reintroduce by introducing a default "children" directory.

--- DefaultPalette/Create new project.ps1
+++
@@ -1,4 +0,0 @@
-function Invoke-Command {
-    param($project, $spawnWt, $projectPath, $wtLocated)
-    . $PSScriptRoot\..\src\createNewProject.ps1
-}
--- src/createNewProject.ps1
+++
@@ -1,7 +0,0 @@
-cls # Pour effacer le "Executing"
-Write-Host "Project name: " -NoNewLine
-$newProject = Read-Host
-mkdir ..\$newProject
-New-Desktop | Set-DesktopName -Name $newProject
-Switch-Desktop -Desktop $newProject
-& $env:ahk_wPD "$PSScriptRoot\focusTermStandalone.ahk"
