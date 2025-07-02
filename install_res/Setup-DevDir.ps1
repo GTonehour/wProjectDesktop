b83f2a5747f7c29745747181818507be0492790e
GTonehour <maxime@gtonehour.com>
2025-07-02 18:08:12 +0200

    Prevent testing from requiring installing

--- install_res/Setup-DevDir.ps1
+++ install_res/Setup-DevDir.ps1
@@ -3,3 +3,12 @@ if (-not (Test-Path $stateFolder)) {
    New-Item -Path $stateFolder -ItemType Directory -Force
 	Out-File -FilePath "$stateFolder\CurrentProject.txt"
 }
+
+# To prevent installing from being a prerequisite to testing.
+$configFolder = Join-Path $PSScriptRoot .. devModeConfig
+if (-not (Test-Path $configFolder)) {
+	New-Item -Path $configFolder -ItemType Directory -Force
+	New-Item -Path $configFolder\Palette -ItemType Directory -Force
+	Out-File -FilePath "$configFolder\configPath.txt"
+	Out-File -FilePath "$configFolder\projects.json"
+}
