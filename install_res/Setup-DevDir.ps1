8c6267e4dc16f7274e6cd493dfcb6b3d34e1b8bc
Romancier <romancier1844@gmail.com>
2025-07-07 10:30:30 +0200

    Fix DevMode

--- install_res/Setup-DevDir.ps1
+++ install_res/Setup-DevDir.ps1
@@ -1,11 +1,11 @@
-$stateFolder = Join-Path $PSScriptRoot .. State
+$stateFolder = Join-Path $PSScriptRoot .. | Join-Path -ChildPath State
 if (-not (Test-Path $stateFolder)) {
    New-Item -Path $stateFolder -ItemType Directory -Force
 	Out-File -FilePath "$stateFolder\CurrentProject.txt"
 }
 
 # To prevent installing from being a prerequisite to testing.
-$configFolder = Join-Path $PSScriptRoot .. devModeConfig
+$configFolder = Join-Path $PSScriptRoot .. | Join-Path -ChildPath devModeConfig
 if (-not (Test-Path $configFolder)) {
 	New-Item -Path $configFolder -ItemType Directory -Force
 	New-Item -Path $configFolder\Palette -ItemType Directory -Force
