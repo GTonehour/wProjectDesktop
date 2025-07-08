57862d713ff1d0999ce46be510bbc501158be510
GTonehour <maxime@gtonehour.com>
2025-07-08 11:58:01 +0200

    Fix devmode setup

--- install_res/Setup-DevDir.ps1
+++ install_res/Setup-DevDir.ps1
@@ -1,6 +1,7 @@
 $stateFolder = Join-Path $PSScriptRoot .. State
 if (-not (Test-Path $stateFolder)) {
    New-Item -Path $stateFolder -ItemType Directory -Force
+   New-Item -Path $stateFolder\MRU -ItemType Directory -Force
 	Out-File -FilePath "$stateFolder\CurrentProject.txt"
 }
 
