8175704b83b06b153c5c96d70b82e7dc8aaff912
Maxime Milhe <maxime@gtonehour.com>
2025-07-15 17:59:58 +0200

    Support paths not found

--- src/ProjectUtils.ps1
+++ src/ProjectUtils.ps1
@@ -13,20 +13,24 @@ $config = Get-Content $configPath | ConvertFrom-Json
     $projectList = @()
     foreach ($item in $config) {
         $expandedPath = Invoke-Expression "`"$($item.Path)`""
-        if ($item.children -eq $true) {  # Fixed condition
-            $subfolders = Get-ChildItem -Path $expandedPath -Directory
-            foreach ($sub in $subfolders) {
-                $projectList += [PSCustomObject]@{Name = $sub.Name; Path = $sub.FullName}
+        $name = $item.Label
+        if (-not $name) {
+            $name = Split-Path $expandedPath -Leaf
+        }
+        if ($expandedPath){ # The path may be a environment variable, null in some environments.
+            if ($item.children -eq $true) {  # Fixed condition
+                $subfolders = Get-ChildItem -Path $expandedPath -Directory
+                foreach ($sub in $subfolders) {
+                    $projectList += [PSCustomObject]@{Name = $sub.Name; Path = $sub.FullName}
+                }
+            } else {
+                if (-not (Test-Path $expandedPath)) {
+                    $name += ' (not found)'
+                }
+                $projectList += [PSCustomObject]@{Name = $name; Path = $expandedPath}
             }
         } else {
-            $name = $item.Label
-            if (-not $name) {
-                $name = Split-Path $expandedPath -Leaf
-            }
-            if (-not (Test-Path $expandedPath)) {
-                $name += ' (not found)'
-            }
-            $projectList += [PSCustomObject]@{Name = $name; Path = $expandedPath}
+            $projectList += [PSCustomObject]@{Name = "$name (not found)"}
         }
     }
     return $projectList
