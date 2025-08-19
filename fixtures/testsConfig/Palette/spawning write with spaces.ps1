a982cb439af80b0fdc51cdbf67087b707df5d4dc
Maxime Milhe <maxime@gtonehour.com>
2025-08-19 17:58:59 +0200

    Move testsPalette to testsConfig

--- fixtures/testsConfig/Palette/spawning write with spaces.ps1
+++ fixtures/testsConfig/Palette/spawning write with spaces.ps1
@@ -2,6 +2,9 @@ param(
     [string]$project,
     [string]$projectPath
 )
+
+Write-Host "📤" # See "simple write...": printing it in "spawning..." will test if we have such a powershell version *in DevModeConfig*
+
 $testFile = Join-Path $env:TEMP "pester-$project.txt"
 "Test executed at $(Get-Date)" | Out-File -FilePath $testFile -Encoding UTF8
 Write-Host "Test file created: $testFile"
--- tests/Palette.Tests.ps1
+++ tests/Palette.Tests.ps1
@@ -1,7 +1,9 @@
 BeforeAll {
-    . $PSScriptRoot\..\src\Palette.ps1
+    $wPdDir = "$PSScriptRoot\.."
+    . $wPdDir\src\Palette.ps1
     # On veut pouvoir tester sans avoir installé (aussi pour pouvoir automatiser les tests sur des serveurs Linux). Donc on veut pouvoir mock Get-ConfigPath vers un dossier créé par Setup-DevDir. Mais Get-ConfigPath est appelé par Get-ProjectList, qui est hors du périmètre du mock. Donc on doit dot-sourcer avant, c'est à dire ici.
-    . $PSScriptRoot\..\src\ProjectUtils.ps1
+    . $wPdDir\src\ProjectUtils.ps1
+    . $wPdDir\src\Show-Term.ps1
 }
 
 Describe 'Palette' {
@@ -14,8 +16,9 @@ Describe 'Palette' {
             return 'simple_print.ps1'
         }
         Mock Hide-Term { }
-		Mock Get-ConfigPath { return Join-Path $PSScriptRoot .. DevModeConfig }
+		Mock Get-ConfigPath { return Join-Path $wPdDir fixtures testsConfig }
 		Mock cls { }
+        $commands = Get-PaletteCommands -wPdDir $wPdDir # AFTER having mocked Get-ConfigPath, because we want testsConfig
     }
 
     AfterEach {
@@ -23,50 +26,39 @@ Describe 'Palette' {
         Get-ChildItem -Path $env:TEMP -Filter "pester-*.txt" | Remove-Item -Force -ErrorAction SilentlyContinue
     }    
     
-    It 'executes simple script with <Terminal>)' -ForEach $terminals {
-        $commands = Get-PaletteCommands -wPdDir (Join-Path $PSScriptRoot ..) -loadTestsPalette $true
-        $result = Invoke-SelectedCommand -selectedCommand 'simple_print' -commands $commands -project "Test" -projectPath "." -Terminal alacritty
+    It 'executes simple script with <Terminal>' -ForEach $terminals {
+        $result = Invoke-SelectedCommand -selectedCommand 'simple_print' -commands $commands -project "Test" -projectPath "." -Terminal $Terminal
         $result | Should -Be 'PALETTE_TEST_SUCCESS'
     }
     
-    It 'script containing space in name spawns and executes' -ForEach $terminals {
+    It 'script containing space in name spawns <Terminal> and executes' -ForEach $terminals {
         $nonce = $(New-Guid)
-        
-        $commands = Get-PaletteCommands -wPdDir (Join-Path $PSScriptRoot ..) -loadTestsPalette $true
-        Write-Host 1
-        Invoke-SelectedCommand -selectedCommand 'spawning write with spaces' -commands $commands -project $nonce -projectPath "." -Terminal alacritty
-        Write-Host 2
-        
+        Invoke-SelectedCommand -selectedCommand 'spawning write with spaces' -commands $commands -project $nonce -projectPath (Join-Path $wPdDir fixtures "path with spaces") -Terminal $Terminal
         Start-Sleep -Seconds 3
-        
         Test-Path (Join-Path $env:Temp "pester-$nonce.txt") | Should -Be $true
      }
 
     It 'doesnt get spaces mangled through the parsing of wt plus <Terminal>' -ForEach $terminals {
         $nonce = $(New-Guid)
         $tempRunnerScript = Join-Path $env:TEMP "pester-runner.ps1"
-        # Resolve the absolute path to the project root for the external script
-        $projectRootPath = Join-Path $PSScriptRoot ..
 
-        # This is the content of the script that 'wt' will execute.
-        # It needs absolute paths because its execution location is different.
-        # Note the backticks (`) before `$commands` and `$true` to ensure they are
-        # treated as literal text in the script file, not expanded immediately.
+        # To "inherit" the Get-Config mocking in $tempRunnerScript
+        $commandsJson = $commandsJson = [System.Management.Automation.PSSerializer]::Serialize($commands)
+        
         $scriptContent = @"
-. '$projectRootPath\src\Palette.ps1'
-. '$projectRootPath\src\ProjectUtils.ps1'
-
-`$commands = Get-PaletteCommands -wPdDir '$projectRootPath' -loadTestsPalette `$true
+# Here in addition to "Before", because this one will execute in our spawned terminal
+. '$wPdDir\src\Palette.ps1'
+. '$wPdDir\src\ProjectUtils.ps1'
+`$commands = [System.Management.Automation.PSSerializer]::Deserialize('$commandsJson')
 Invoke-SelectedCommand -selectedCommand 'spawning write with spaces' -commands `$commands -project '$nonce' -projectPath "." -Terminal $Terminal
 "@
         # Create the temporary runner script
         $scriptContent | Out-File -FilePath $tempRunnerScript -Encoding UTF8
         
         # The command to execute our runner script in a new, non-focused wt process
-        $wtCommand = "wt powershell -File $tempRunnerScript"
-        Invoke-Expression $wtCommand
+        Start-WindowsTerminal -ScriptPath $tempRunnerScript -WithPositioning $false
 
-        Start-Sleep -Seconds 5 # 3 was enough on some devices, but not on others. 6 is enough.
+        Start-Sleep -Seconds 7 # 5 wasn't enough on some devices.
 
         # Assert that the final output file was created by the nested process
         Test-Path (Join-Path $env:Temp "pester-$nonce.txt") | Should -Be $true
