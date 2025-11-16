f64d1dc451e075a8295d3dd182c80fe4cdda6ca3
Maxime Milhe <maxime@gtonehour.com>
2025-11-16 12:53:53 +0100

    Make pre-ahk bat script faster

--- src/desktop_matches_state.bat
+++ src/desktop_matches_state.bat
@@ -4,12 +4,7 @@ setlocal enabledelayedexpansion
 REM Get current desktop number
 call "%~dp0virtualdesktop_exe.bat"
 
-REM Get current desktop output
-for /f "tokens=*" %%i in ('"%exe_path%" /GetCurrentDesktop 2^>nul') do set "desktop_output=%%i"
-
-REM Extract desktop name from single quotes using string manipulation
-set "desktop_line=%desktop_output%"
-for /f "tokens=2 delims='" %%a in ("%desktop_line%") do set "currentDesktopName=%%a"
+for /f "tokens=2 delims='" %%a in ('"C:\Users\mmi2\projects\bin\VirtualDesktop.exe" /GetCurrentDesktop 2^>nul') do set "currentDesktopName=%%a"
 
 REM Read current project from file
 if not exist "..\State\currentProject.txt" (
