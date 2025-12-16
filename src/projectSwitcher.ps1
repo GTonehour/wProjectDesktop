b850748fa2fc22bde78e8679530b81e26942181f
Maxime Milhe <maxime@gtonehour.com>
2025-12-16 15:15:33 +0100

    Allow escaping fzf in systems where ErrorActionPreference is "Stop"

--- src/projectSwitcher.ps1
+++ src/projectSwitcher.ps1
@@ -17,10 +17,24 @@ $projectList = $projectList | ForEach-Object {
     $_ | Add-Member -NotePropertyName newName -NotePropertyValue $newName -PassThru
 }
 
-$selection = $projectList |
-    Sort-Object -Property @{Expression="Opened"; Descending=$true}, @{Expression="Name"} |
-    Select-Object -ExpandProperty newName |
-    fzf.exe --prompt "Open project " # J'enlève le "--bind one:accept". En particulier à l'intallation il n'y avait que le seul projet "config", donc le choix du projet y emmenait avant d'avoir montré ce (seul) choix.
+try { # For ESCape to continue without in systems set with $ErrorActionPreference "Stop".
+    $selection = $projectList |
+        Sort-Object -Property @{Expression="Opened"; Descending=$true}, @{Expression="Name"} |
+        Select-Object -ExpandProperty newName |
+        fzf.exe --prompt "Open project " # J'enlève le "--bind one:accept". En particulier à l'intallation il n'y avait que le seul projet "config", donc le choix du projet y emmenait avant d'avoir montré ce (seul) choix.
+}
+catch [System.Management.Automation.NativeCommandExitException] {
+    # Check if the specific exit code was 130 (User pressed Esc)
+    if ($_.Exception.ExitCode -eq 130) {
+        $Name = $null
+        # Optionally explicitly reset the error state, though capturing handles it
+        $global:LASTEXITCODE = 130 
+    }
+    else {
+        # If fzf failed for a reason other than ESC, re-throw the error
+        throw $_
+    }
+}        
 
 if (-not [string]::IsNullOrEmpty($selection)) { # Si on a fait échap, ne faisons rien d'autre.
     $project = $projectList | Where-Object { $_.newName -eq $selection }
